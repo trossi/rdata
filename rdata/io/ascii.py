ffecd84497273d76a91de976209c6c6a79fea9d7
Tuomas Rossi <tuomas.rossi@csc.fi>
2023-11-24 16:31:34 +0200

    Add xdr and ascii writers

--- rdata/io/ascii.py
+++ rdata/io/ascii.py
@@ -1,5 +1,9 @@
 from __future__ import annotations
 
+import string
+import numpy as np
+
+
 from typing import (
     TextIO,
 )
@@ -10,6 +14,8 @@ from rdata.parser._parser import (
     Parser,
     RData,
 )
+
+from .base import Writer
 
 
 R_INT_NA = 'NA' # noqa: WPS432
@@ -56,3 +62,44 @@ class ParserASCII(Parser):
         return rdata
 
 
+class WriterASCII(Writer):
+    """Writer for ASCII format files."""
+
+    def __init__(
+        self,
+        file: TextIO,
+    ) -> None:
+        self.file = file
+
+    def _writeline(self, line) -> None:
+        """Write a line with trailing \\n"""
+        self.file.write(f'{line}\n')
+
+    def write_magic(self):
+        self._writeline('A')
+
+    def write_nullable_bool(self, value):
+        if value is None or np.ma.is_masked(value):
+            self._writeline('NA')
+        else:
+            self.write_int(int(value))
+
+    def write_nullable_int(self, value):
+        if value is None or np.ma.is_masked(value):
+            self._writeline('NA')
+        else:
+            self._writeline(value)
+
+    def write_double(self, value):
+        self._writeline(str(value))
+
+    def write_string(self, value: bytes):
+        self.write_int(len(value))
+
+        # This line would produce byte representation in hex such as '\xc3\xa4':
+        # value = value.decode('latin1').encode('unicode_escape').decode('ascii')
+        # but we need to have the equivalent octal presentation '\303\244'.
+        # So, we use a somewhat manual conversion instead:
+        value = ''.join(chr(byte) if chr(byte) in string.printable else rf'\{byte:03o}' for byte in value)
+
+        self._writeline(value)
